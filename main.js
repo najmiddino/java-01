ee90ad50bf4c4e4026e318dba9143424afcef63c
najmiddino <164999098+najmiddino@users.noreply.github.com>
2024-05-25 13:59:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let a = prompt("ismingizni kiriting")
+let b = prompt("Familyangizni kiriting")
+let d = prompt("yoshingizni kiriting")
+let c = prompt("yoqtiradigan faningizni kiriting")
+let g = prompt("yashaydigan tumaningizni kiriting")
+
+alert("malumotlarni kiritkaningiz uchun raxmat")
